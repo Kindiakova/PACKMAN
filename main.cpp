15f30976b8eeb924ef9edb04a5937db773030c2e
Diana Kindiakova <75929765+Kindiakova@users.noreply.github.com>
2023-11-10 15:08:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+void generate_map();
+
 int main(int argc, char *argv[])
 {
+    //generate_map();
     QApplication a(argc, argv);
     MainWindow w;
     w.setStyleSheet("QMainWindow {background: 'black';}");
     w.show();
     return a.exec();
 }
+
